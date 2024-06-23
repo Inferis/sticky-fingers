c29b12a784e07cd3bdea4baeee13acaf543a6fbe
Tom <tom@inferis.org>
2024-06-23 11:04:44 +0200

    Added XP orb processing.
    Also, moved the "around player" code into EntityVacuum.

--- src/main/java/org/inferis/stickyfingers/EntityVacuum.java
+++ src/main/java/org/inferis/stickyfingers/EntityVacuum.java
@@ -5,8 +5,10 @@ import org.inferis.stickyfingers.items.MagnetItem.Mode;
 
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.ExperienceOrbEntity;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.predicate.entity.EntityPredicates;
 import net.minecraft.world.World;
 
 public class EntityVacuum {
@@ -23,26 +25,59 @@ public class EntityVacuum {
             return;
         }
 
-        if (!(entity instanceof ItemEntity itemEntity) || entity.isRemoved()) {
+        if (entity.isRemoved()) {
+            return;
+        }
+
+        // we can only process item and xp orb entities
+        var isItemEntity = entity instanceof ItemEntity;
+        var isXpEntity = entity instanceof ExperienceOrbEntity;
+        if (!isItemEntity && !isXpEntity) {
             return;
         }
 
         for (var player: world.getPlayers()) {
-            if (tryVacuum(itemEntity, (PlayerEntity)player, false)) {
+            if (tryVacuum(entity, (PlayerEntity)player, true)) {
                 return;
             }
         }
     }
 
-    public static boolean tryVacuum(ItemEntity itemEntity, PlayerEntity player, boolean resetDelay) {
+    public static void tryVacuumAround(PlayerEntity player) {
+        // find item entities around the player first
+        var itemEntities = player.getWorld().getEntitiesByClass(
+            ItemEntity.class, 
+            player.getBoundingBox().expand(EntityVacuum.MAX_DISTANCE), 
+            EntityPredicates.VALID_ENTITY
+        );
+        for (var itemEntity: itemEntities) {
+            EntityVacuum.tryVacuum(itemEntity, player, false);
+        }
+
+        // then xp orbs
+        var xpEntities = player.getWorld().getEntitiesByClass(
+            ExperienceOrbEntity.class, 
+            player.getBoundingBox().expand(EntityVacuum.MAX_DISTANCE), 
+            EntityPredicates.VALID_ENTITY
+        );
+        for (var xpEntity: xpEntities) {
+            EntityVacuum.tryVacuum(xpEntity, player, false);
+        }
+    }
+
+    // Try to vacuum/process one entity for a player.
+    public static boolean tryVacuum(Entity entity, PlayerEntity player, boolean resetDelay) {
         var mode = MagnetItem.magnetModeOfPlayer(player);
         if (!player.isSpectator() && mode == Mode.ACTIVE) {
             var maxDistance = MAX_DISTANCE;
-            if (player.isInRange(itemEntity, maxDistance)) {
-                if (resetDelay) { 
+            // if the player is in range of the entity, pretend they ran
+            // into it so they pick it up. For freshly spawned items, we also
+            // need to reset the 
+            if (player.isInRange(entity, maxDistance)) {
+                if (entity instanceof ItemEntity itemEntity) { 
                     itemEntity.setPickupDelay(0);
                 }
-                itemEntity.onPlayerCollision(player);
+                entity.onPlayerCollision(player);
                 return true;
             }
         }
--- src/main/java/org/inferis/stickyfingers/mixin/PlayerMoveMixin.java
+++ src/main/java/org/inferis/stickyfingers/mixin/PlayerMoveMixin.java
@@ -6,6 +6,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.entity.ExperienceOrbEntity;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import net.minecraft.predicate.entity.EntityPredicates;
@@ -17,13 +18,6 @@ public class PlayerMoveMixin {
     @Inject(at = @At("RETURN"), method = "onPlayerMove(Lnet/minecraft/network/packet/c2s/play/PlayerMoveC2SPacket;)V")
     public void onPlayerMove(PlayerMoveC2SPacket packet, CallbackInfo callbackInfo) {
         var player = ((ServerPlayNetworkHandler)(Object)this).player;
-        var entities = player.getWorld().getEntitiesByClass(
-            ItemEntity.class, 
-            player.getBoundingBox().expand(EntityVacuum.MAX_DISTANCE), 
-            EntityPredicates.VALID_ENTITY
-        );
-        for (var itemEntity: entities) {
-            EntityVacuum.tryVacuum(itemEntity, player, true);
-        }
+        EntityVacuum.tryVacuumAround(player);
     }
 }
