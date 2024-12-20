effac78278bd20f282a506363aba0116b8424cca
Tom <tom@inferis.org>
2024-12-20 15:44:25 -0800

    Update to 1.21.4.

--- src/main/java/org/inferis/stickyfingers/items/ModItems.java
+++ src/main/java/org/inferis/stickyfingers/items/ModItems.java
@@ -7,20 +7,31 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
     public static Item MAGNET;
 
-    private static <T extends Item> T registerItem(String name, T item) {
+    interface ItemRegistration<T> {
+        T makeItem(RegistryKey<Item> key);
+    }
+
+    private static <T extends Item> T registerItem(String name, ItemRegistration<T> itemRegistration) {
         StickyFingers.LOGGER.info("Registering item: " + StickyFingers.MODID + ":" + name);
-        return Registry.register(Registries.ITEM, Identifier.of(StickyFingers.MODID + ":" + name), item);
+        Identifier identifier = Identifier.of(StickyFingers.MODID, name);
+        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, identifier);
+        T item = itemRegistration.makeItem(key);
+        return Registry.register(Registries.ITEM, identifier, item);
     }
 
     public static void registerItems() {
         MAGNET = registerItem(
             MagnetItem.IDENTIFIER, 
-            new MagnetItem(new Item.Settings())
+            key -> {
+                return new MagnetItem(new Item.Settings().registryKey(key));
+            }
         );
 
         // Add magnet to "Tools" group.
